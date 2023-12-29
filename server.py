1a6f296f7c6aff562b2242d6e9714c244aed0905
IrynaBarnych <iryna.barnych@gmail.com>
2023-12-29 21:01:05 +0200

    виправила на 8081

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import threading
 from googletrans import Translator
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("127.0.0.1", 8080))
+server_socket.bind(("127.0.0.1", 8081))
 server_socket.listen(5)
 print("Очікуємо підключення...")
 
